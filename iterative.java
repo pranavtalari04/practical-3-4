c21d4b69ecd9a965f36046eed4095903eaee7a84
pranavtalari04 <pranavtalari04@gmail.com>
2026-08-19 10:45:49 +0530

    Implement iterative method for calculating factorial

---
+++ iterative.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class iterativeMethod {
+
+    public static long factorial(int n) {
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial of " + n + " = " + factorial(n));
+
+        sc.close();
+    }
+}
